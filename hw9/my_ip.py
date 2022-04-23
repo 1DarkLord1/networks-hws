73c1b4a96d33ef762ec4498b1a781df565cadb2f
1DarkLord1 <camm2016@yandex.ru>
2022-04-23 23:41:27 +0300

    fix my_ip.py

--- hw9/my_ip.py
+++ hw9/my_ip.py
@@ -2,6 +2,10 @@ import netifaces as ni
 
 
 if __name__ == '__main__':
-    info = ni.ifaddresses('wlp0s20f3')[ni.AF_INET][0]
-    print(f'Addr: {info["addr"]}')
-    print(f'Netmask: {info["netmask"]}')
+    for interface in ni.interfaces():
+        print(interface)
+
+        info = ni.ifaddresses(interface)[ni.AF_INET][0]
+
+        print(f'Addr: {info["addr"]}')
+        print(f'Netmask: {info["netmask"]}\n')
